697ac26f067492f8a2809c4246227baf76c76468
oddduckden <dkireev.71@gmail.com>
2021-04-01 19:08:19 +0300

    task4

--- task4.py
+++ task4.py
@@ -7,8 +7,22 @@ Four — 4
 Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные. При этом английские
 числительные должны заменяться на русские. Новый блок строк должен записываться в новый текстовый файл.
 """
-dictionary =
-my_f = open("text.txt", "r")
-for line in my_f:
-    print(line)
-my_f.close()
+
+
+def number_dictionary(number):
+    dictionary = {1: 'Один', 2: 'Два', 3: 'Три', 4: 'Четыре'}
+    return dictionary[number]
+
+
+def parser(next_string: str):
+    el = next_string[:-1].split() if next_string[:-1] == '\n' else next_string.split()
+    el[0] = number_dictionary(int(el[-1]))
+    return ' '.join(el)
+
+
+in_f = open('numbers.txt', 'r')
+out_f = open('result.txt', 'w')
+for line in in_f:
+    out_f.writelines(parser(line) + '\n')
+in_f.close()
+out_f.close()
